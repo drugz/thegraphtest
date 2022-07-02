1668529acd8a8a757f9285d635ee94c776f44612
Oleg Korzhanov <ok@xfactorial.com>
2022-07-03 03:47:23 +0400

    add vercel adapter

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 import watchAndRun from '@kitql/vite-plugin-watch-and-run'
@@ -11,8 +12,22 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		// adapter: adapter(),
 
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+	  
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+	  
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		  }),
 		vite: {
 			plugins: [
 				watchAndRun([
