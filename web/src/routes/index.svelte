a70f955aa664447172d21134b10c30f27ac42191
Oleg Korzhanov <ok@xfactorial.com>
2022-07-02 14:02:06 +0400

    import grpah to index

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script lang="ts" type="module">
+import { dataSource } from '@graphprotocol/graph-ts'
+dataSource.network()
+dataSource.address()
+    </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
